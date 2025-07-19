e0e437ff8f67fb15665c713f95948664af3b29e4
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-07-19 21:48:52 +0200

    homepage nearly finished

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -45,6 +45,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :description, :date, :start_time, :end_time)
+    params.require(:event).permit(:card_image, :title, :description, :date, :start_time, :end_time)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_18_105447) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_19_163917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,6 +83,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_18_105447) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
+    t.boolean "super_admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
